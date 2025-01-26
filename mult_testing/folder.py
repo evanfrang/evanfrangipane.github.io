d54e5be64be4cdefbf20643914773fff3fac0866
evanfrang <frangipane.evan@gmail.com>
2025-01-26 15:31:39 -0800

    just leaving the code for now

--- mult_testing/folder.py
+++ mult_testing/folder.py
@@ -10,11 +10,11 @@ code_block_pattern = r"```(\w+)\n(.*?)```"
 # Wrap each code block with <details> and <summary>
 wrapped_content = re.sub(
     code_block_pattern,
-    r"<details>\n<summary>Click to show \1 code</summary>\n\n```\1\n\2```\n\n</details>",
+    r"fail",
     content,
     flags=re.DOTALL,
 )
 
 # Output to a new Markdown file
-with open("README.md", "w", encoding="utf-8") as file:
+with open("README_2.md", "w", encoding="utf-8") as file:
     file.write(wrapped_content)
